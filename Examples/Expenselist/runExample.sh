1d2578ab7f75beda4c9ded69d078514fe905584c
BFG <Tom.Fredrik@blenning.no>
2019-05-01 10:27:36 +0000

    Moved resource into common area

--- Examples/Expenselist/runExample.sh
+++ Examples/Expenselist/runExample.sh
@@ -1,1 +1,1 @@
-PYTHONPATH=../../lib ./expenseForm.py --verbose -o test.ods Expenselist.ots 'Getting drunk' '' 'Beer' '123' '' 'Spirits' '90'
+PYTHONPATH=../../lib ./expenseForm.py --verbose -o test.ods ../resources/Expenselist.ots 'Getting drunk' '' 'Beer' '123' '' 'Spirits' '90'
